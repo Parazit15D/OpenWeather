2130f34ddf2577451e8958886634d1df2d3e05d6
Parazit15D <igorlyons@gmail.com>
2022-12-15 14:29:40 +0200

    Save

---
+++ styles/weather.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+
+    contentWeather: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+
+    textWeather: {
+        fontSize: 15,
+        color: 'white',
+        justifyContent: 'center'
+    },
+
+    icon: {
+        width: 25,
+        height: 25,
+    }
+});
+
+
+
+
+
+
+
+
+
